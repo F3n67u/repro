ab2db434ced95db73d54a9a4b5f3edaaff0a9a99
yufeng.freeman <yufeng.freeman@bytedance.com>
2022-03-10 09:38:30 +0800

    clean up

--- test.js
+++
@@ -1,8 +0,0 @@
-test.each([
-  {a: 1, b: 1, expected: 2},
-  {a: 1, b: 2, expected: 3},
-  {a: 2, b: 1, expected: 3},
-])('.add($a, $b)', ({a, b, expected}) => {
-  expect(a + b).toBe(expected);
-});
-
